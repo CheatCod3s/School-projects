7b27d60adbc081ca5a09071a64f5de117cd0fcbd
Sandra Raitaniemi <sandra.raitaniemi@student.tut.fi>
2016-12-24 14:06:03 +0200

    Harjoitustyo-05 valmis.

---
+++ harjoitustyo-05/tiedoston_luku.hh
@@ -0,0 +1,20 @@
+// Harjoitustyö-05
+// Sandra Raitaniemi, 254081
+// sandra.raitaniemi@student.tut.fi
+
+// Moduuli tiedoston_luku / tiedosto: tiedoston_luku.hh
+// Moduulin tiedoston_luku esittelytiedosto:
+// sisältää esittelyn tiedoston lukuun tarvittavalle funktiolle.
+
+#ifndef TIEDOSTON_LUKU_HH
+#define TIEDOSTON_LUKU_HH
+#include "lista.hh"
+#include <fstream>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool lue_tiedosto(ifstream& virta, Lista& lista);
+
+#endif // TIEDOSTON_LUKU_HH
